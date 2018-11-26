b5d568cc0947174eac6c15ba58405dfdc6a2d0cd
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-25 22:27:05 -0600

    Update ToDoListActivity.kt
    Add Kdoc to ToDoListActivity.kt

--- app/src/main/java/me/alfredobejarano/livedataconverterdemo/ToDoListActivity.kt
+++ app/src/main/java/me/alfredobejarano/livedataconverterdemo/ToDoListActivity.kt
@@ -10,11 +10,31 @@ import androidx.recyclerview.widget.RecyclerView
 import me.alfredobejarano.livedataconverterdemo.utilities.Injector
 import me.alfredobejarano.livedataconverterdemo.viewmodel.ToDoListViewModel
 
+/**
+ * Activity that displays a List of ToDo objects inside a RecyclerView.
+ */
 class ToDoListActivity : AppCompatActivity() {
+    /**
+     * Defines the root view of this activity.
+     */
     private lateinit var mRootView: RecyclerView
+    /**
+     * Defines the ViewModel for this UI controller.
+     */
     private lateinit var mViewModel: ToDoListViewModel
+    /**
+     * Uses dependency inversion for retrieving a ViewModel Factory for this UI controller ViewModel.
+     */
+    private val mViewModelFactory = Injector.toDoListViewModelFactory
 
+    /**
+     * Creates this activity, retrieves the ViewModel for this UI
+     * controller and then proceeds to fetch the list of ToDo objects.
+     *
+     * @param savedInstanceState The saved state of a previous instance of this activity.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Execute the super method.
         super.onCreate(savedInstanceState)
         // Initialize the root view.
         mRootView = RecyclerView(this).also {
@@ -22,7 +42,7 @@ class ToDoListActivity : AppCompatActivity() {
             it.layoutManager = LinearLayoutManager(this)
         }
         // Retrieve a ViewModel for this activity using dependency inversion.
-        mViewModel = ViewModelProviders.of(this, Injector.toDoListViewModelFactory)[ToDoListViewModel::class.java]
+        mViewModel = ViewModelProviders.of(this, mViewModelFactory)[ToDoListViewModel::class.java]
         // Sets the activity root as a RecyclerView.
         setContentView(mRootView)
         // Retrieve the To Do list.
