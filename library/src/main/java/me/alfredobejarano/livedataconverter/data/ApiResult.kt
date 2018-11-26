7cf63eace938bc077bbcff583d878273cc6611ad
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-25 22:17:34 -0600

    Create ApiResult.kt
    Model class that will encapsulate the result of a Retrofit API call, it can contain the body (result of the API call) or any exception that happens in the execution of said call

---
+++ library/src/main/java/me/alfredobejarano/livedataconverter/data/ApiResult.kt
@@ -0,0 +1,24 @@
+package me.alfredobejarano.livedataconverter.data
+
+/**
+ *
+ * Model class containing the results of a API call from Retrofit.
+ *
+ * @author Alfredo Bejarano
+ * @since November 25, 2018 - 20:09
+ * @version 1.0
+ **/
+data class ApiResult<T>(
+    /**
+     * HTTP code result from the call.
+     */
+    val code: Int,
+    /**
+     * Error that happened while executing the call. (if something went wrong).
+     */
+    val error: Throwable?,
+    /**
+     * The result from the API call.
+     */
+    val body: T?
+)
