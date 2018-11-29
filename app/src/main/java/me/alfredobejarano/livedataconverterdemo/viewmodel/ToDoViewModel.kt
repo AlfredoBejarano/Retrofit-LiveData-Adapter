f8307153d1f53f510b4f8a805b89e3c34d767cc6
Alfredo Bejarano <alfredocbejarano@gmail.com>
2018-11-29 16:13:17 -0600

    Create ToDoViewModel.kt
    ViewModel class that receives UI events to display a ToDo object

---
+++ app/src/main/java/me/alfredobejarano/livedataconverterdemo/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,25 @@
+package me.alfredobejarano.livedataconverterdemo.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import me.alfredobejarano.livedataconverterdemo.data.ToDo
+import me.alfredobejarano.livedataconverterdemo.data.ToDoRepository
+
+/**
+ *
+ * ViewModel that intercepts UI events and uses a [repository class][ToDoRepository]
+ * to provide data for the UI using observation via [LiveData] objects.
+ *
+ * @author Alfredo Bejarano
+ * @since November 29, 2018 - 16:09
+ * @version 1.0
+ **/
+class ToDoViewModel(private val repository: ToDoRepository) : ViewModel() {
+    /**
+     * Retrieves a ToDO from the cloud by its id.
+     * @param toDoId Id of the ToDo object.
+     * @return [LiveData] object containing the [ToDo].
+     */
+    fun fetchToDo(toDoId: Int): LiveData<ToDo> =
+        repository.fetchToDo(toDoId)
+}
